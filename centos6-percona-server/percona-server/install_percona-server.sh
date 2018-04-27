0731c37fd963cfaf74281aadb174993eeb75b5e8
ppabc8 <ppabc8@gmail.com>
2018-04-27 14:51:03 +0800

    update

--- centos6-percona-server/percona-server/install_percona-server.sh
+++ centos6-percona-server/percona-server/install_percona-server.sh
@@ -2,7 +2,7 @@
 yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-4/percona-release-0.1-4.noarch.rpm
 yum install -y epel-release
 yum install -y libev
-yum install -y Percona-Server-shared-56  Percona-Server-client-56 Percona-Server-devel-56 Percona-Server-server-56 openssh-server openssh-clients percona-xtrabackup-24
+yum install -y Percona-Server-shared-56  Percona-Server-client-56 Percona-Server-devel-56 Percona-Server-server-56 percona-xtrabackup-24 percona-toolkit
 
 ln -s /usr/lib64/libperconaserverclient_r.so.18 /usr/lib64/libperconaserverclient_r.so
 chown -R mysql:mysql /var/lib/mysql
